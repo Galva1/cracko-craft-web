79ec8b184d725d437f48625a7c78a247cf492f93
Galvão <gabrielgm07@hotmail.com>
2025-04-19 22:43:02 -0300

    Correção em erro de console + ajuste no header

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export function Header(){
     return (
-        <header className="absolute w-full flex items-center justify-between p-4 bg-gray-800 text-white">
+        <header className="flex items-center justify-between p-4 bg-gray-800 text-white">
             <Link href='/'><h1 className="text-2xl font-bold">Cracko Craft</h1></Link>
             <nav>
                 <ul className="flex space-x-4">
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -15,14 +15,14 @@ export default function Sobre(){
         height: 'auto',
     }
 
-    const titulo1Ref = useRef(null);
-    const titulo2Ref = useRef(null);
-    const titulo3Ref = useRef(null);
-    const titulo4Ref = useRef(null);
-    const titulo5Ref = useRef(null);
-    const titulo6Ref = useRef(null);
+    const titulo1Ref = useRef<HTMLHeadingElement>(null);
+    const titulo2Ref = useRef<HTMLHeadingElement>(null);
+    const titulo3Ref = useRef<HTMLHeadingElement>(null);
+    const titulo4Ref = useRef<HTMLHeadingElement>(null);
+    const titulo5Ref = useRef<HTMLHeadingElement>(null);
+    const titulo6Ref = useRef<HTMLHeadingElement>(null);
 
-    const handleClickTitulo = (ref: React.RefObject<HTMLHeadingElement>) => {
+    const handleClickTitulo = (ref: React.RefObject<HTMLHeadingElement | null>) => {
         ref.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
@@ -41,8 +41,8 @@ export default function Sobre(){
                             src="/images/amigoscraft1.png"
                             alt="AmigosCraft1"
                             width={400}
-                            height={0}
-                            className="mt-2"
+                            height={400}
+                            className="mt-2 h-auto w-auto"
                         />
                         <p className="text-lg mt-2">O grupo de AmigosCraft iniciou quando alguns amigos resolveram se juntar em um mundo lan e jogar despretensiosamente Minecraft</p>
                         <p className="text-lg mt-3">Inicialmente começou com os jogadores: Ruancell, PaulinhoIce, Hallbora, Lakewood_Lake e Galva1, tendo algumas vertentes com Fefe, Samuel, PpLucass, entre outros...</p>
@@ -55,18 +55,18 @@ export default function Sobre(){
                             src="/images/amigoscraft2.png"
                             alt="AmigosCraft1"
                             width={800}
-                            height={0}
-                            className="mt-2"
+                            height={800}
+                            className="mt-2 h-auto w-auto"
                         />
                         <p className="text-1 italic">Registro da primeira vila do grupo (o registro final foi perdido, sendo esse semanas antes de fechar o servidor)</p>
                     </div>
-                    <div className="absolute top-1/4 left-1/1 transform -translate-x-1/2 -translate-y-1/2 w-1/4 bg-gray-800 p-6 rounded-lg shadow-lg hover:shadow-2xl transition-all duration-300 hover:w-1/2">
+                    <div className="fixed top-1/4 right-4 w-1/6 bg-gray-800 p-6 rounded-lg shadow-lg hover:shadow-2xl transition-all duration-300 hover:w-1/4 z-50">
                         <ul className="list-disc list-inside text-white">
-                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800">O que é</a></li>
-                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo2Ref)} className="hover:text-cyan-800">AmigosCraft</a></li>
-                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800">Galinheiro</a></li>
-                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800">O inicio</a></li>
-                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800">Edição mods</a></li>
+                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800 cursor-pointer">O que é</a></li>
+                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo2Ref)} className="hover:text-cyan-800 cursor-pointer">AmigosCraft</a></li>
+                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800 cursor-pointer">Galinheiro</a></li>
+                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800 cursor-pointer">O inicio</a></li>
+                            <li className="text-lg"><a onClick={() => handleClickTitulo(titulo1Ref)} className="hover:text-cyan-800 cursor-pointer">Edição mods</a></li>
                         </ul>
                     </div>
                 </div>
