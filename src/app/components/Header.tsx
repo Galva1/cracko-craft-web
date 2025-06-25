d70e88d19f707236a8577fb1e9fc6d03ff224e5b
-set <paulortpraxedes@gmail.com>
2025-06-25 18:22:03 -0300

    Modificando a home e adicionadno svg ao navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,6 +4,38 @@ import React, { useContext } from 'react';
 import { Button } from './button';
 import Image from 'next/image';
 
+const sol = <svg
+xmlns="http://www.w3.org/2000/svg"
+width="1em"
+height="1em"
+viewBox="0 0 24 24"
+fill="none"
+stroke="currentColor"
+stroke-width="2"
+stroke-linecap="round"
+stroke-linejoin="round"
+>
+<circle cx="12" cy="12" r="5" />
+<line x1="12" y1="1" x2="12" y2="3" />
+<line x1="12" y1="21" x2="12" y2="23" />
+<line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+<line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+<line x1="1" y1="12" x2="3" y2="12" />
+<line x1="21" y1="12" x2="23" y2="12" />
+<line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+<line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+</svg>
+
+const lua = <svg
+xmlns="http://www.w3.org/2000/svg"
+width="1em"
+height="1em"
+viewBox="0 0 24 24"
+fill="currentColor"
+>
+<path d="M21 12.79A9 9 0 1 1 11.21 3a7 7 0 0 0 9.79 9.79z" />
+</svg>
+
 
 
 export function Header(){
@@ -16,7 +48,7 @@ export function Header(){
             <nav>
                 
                 <ul className="flex space-x-4">
-                    <Button onClick={toggleTheme}> <p className='hover:text-gray-400'>{theme}</p></Button>
+                    <button onClick={toggleTheme} className='hover:text-gray-400 px-5 py-2 text-base font-bold bg-[#FFA3EC] rounded border-none cursor-pointer'>{theme === 'day' ? sol : lua}</button>
                     <li><Button><Link href='/' className='hover:text-gray-400'>Home</Link></Button></li>
                     <li><Button><Link href='/sobre' className='hover:text-gray-400'>Sobre</Link></Button></li>
                     <li><Button><Link href='/jogadores' className='hover:text-gray-400'>Jogadores</Link></Button></li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 
 import React, { useContext } from "react";
 import { ThemeContext } from "./api/provider/theme-provider";
+import { Button } from "./components/button";
 
 export default function Home() {
 
-  const {theme} = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
   const backgroundImageStyle = {
     backgroundImage: `url(/${theme}wallpaper.png)`,
@@ -15,10 +16,24 @@ export default function Home() {
   };
 
   return (
-    <div style={backgroundImageStyle}>
-      <main className="flex flex-col items-center justify-between p-24 tela-container">
-        <h1 className="text-3xl font-bold">Seja bem vindo ao site oficial do CrackoCraft</h1>
-      </main>
-    </div>
+    <>
+      <div style={backgroundImageStyle} >
+        <main className="flex flex-col items-center justify-between p-12 tela-container">
+          <div className="w-full h-full bg-gray-400/50 p-8" >
+            <div className="text-center relative max-w-6xl mx-auto mt-auto">
+              <h1 className="text-6xl md:text-8xl font-black text-gray-900 dark:text-white mb-6 tracking-tight leading-tight ">
+                <span className="flex">Bem vindo ao CrackoCraft</span>   
+                </h1>
+                <section className="mt-15 flex gap-4 justify-center">
+                  <button className="px-5 py-2 text-base font-bold bg-[#FFA3EC] rounded border-none cursor-pointer transform duration-300 hover:scale-110 ">Baixar Modpack</button>
+                  <button className="px-5 py-2 text-base font-bold bg-[#FFA3EC] rounded border-none cursor-pointer transform duration-300 hover:scale-110">Copiar IP</button>
+                </section>
+            </div>
+          </div>
+        </main>
+
+
+      </div>
+    </>
   );
 }
